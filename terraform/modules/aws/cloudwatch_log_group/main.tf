0a0aafb55593ec5dc2ec9838e15c826e6f853139
SkipEveryLunch <skipeverylunch@outlook.jp>
2026-05-30 21:25:58 +0900

    Firelens用のCWのロググループを追加

--- terraform/modules/aws/cloudwatch_log_group/main.tf
+++ terraform/modules/aws/cloudwatch_log_group/main.tf
@@ -12,3 +12,8 @@ resource "aws_cloudwatch_log_group" "worker" {
   name              = "/ecs/${var.project_name}-worker-${var.env}"
   retention_in_days = var.retention_in_days
 }
+
+resource "aws_cloudwatch_log_group" "log_router" {
+  name              = "/ecs/${var.project_name}-log-router-${var.env}"
+  retention_in_days = var.retention_in_days
+}
